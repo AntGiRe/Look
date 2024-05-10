68ba8a770aa6e5f48a97c6b228753a4d77ceafef
AntGiRe <antgire@gmail.com>
2024-05-10 14:57:37 +0200

    initial state

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class RegisterController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('auth/register');
+    }
+
+    public function store(Request $request)
+    {
+        //dd($request->get('email'));
+
+        $request->request->add([
+            'username' => Str::slug($request->get('username')),
+        ]);
+
+        $request->validate([
+            'name' => 'required|max:64',
+            'username' => 'required|max:24|unique:users|min:3',
+            'email' => 'required|email|max:64|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->get('name'),
+            'username' => Str::slug($request->get('username')),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
