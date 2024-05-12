e3780427cb378f4312e478bec1dafd262d17a462
AntGiRe <antgire02@gmail.com>
2024-05-12 11:02:37 +0200

    some modifs to users feed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.app')
 
-@section('title', 'Your account')
+@section('title', 'Perfil: ' . $user->name)
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class="md:w-8/12 lg:w-6/12 px-5">
+        <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
+            <div class="w-8/12 lg:w-6/12 px-5">
                 <img src="{{ asset('img/user.svg') }}" alt="Imagen usuario" class="w-32 h-32 rounded-full mx-auto">
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5">
-                <p class="text-gray-700 text-2xl">{{ Auth::user()->username }}</p>
+            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center py-10 md:py-10 md:items-start">
+                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">Seguidores</span>
+                </p>
+
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">Siguiendo</span>
+                </p>
+
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">Posts</span>
+                </p>
             </div>
         </div>
     </div>
