af23cee0d50a176f381382e8812c3af34767e22c
AntGiRe <antgire02@gmail.com>
2024-05-13 18:50:13 +0200

    Eloquent relations Post-User

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,9 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'image', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->select(['name', 'username']);
+    }
 }
