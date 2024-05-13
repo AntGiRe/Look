01cccdb4334bfde3c0c03c8d5667bc4b8fd9356f
AntGiRe <antgire02@gmail.com>
2024-05-13 23:40:16 +0200

    Pagination at user feed photos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,4 +28,27 @@
             </div>
         </div>
     </div>
+
+    <section class="container mx-auto mt-10">
+        <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+
+        @if($posts->isEmpty())
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay publicaciones</p>
+        @else
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                @foreach ($posts as $post)
+                    <div>
+                        <a>
+                            <img src="{{ asset('uploads') . '/' . $post->image}}" alt="Imagen del post {{$post->title}}">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+
+            <div class="my-10">
+                {{ $posts->links('pagination::tailwind') }}
+            </div>
+        @endif
+
+    </section>
 @endsection
